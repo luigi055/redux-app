c57b46d772e6c478903218a68c1b380bbb6fa3fb
Pedro La Rosa <luigi5021@gmail.com>
2016-12-23 03:01:46 -0400

    Add support forCHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -7,10 +7,33 @@ console.log('Starting redux example');
 // 1). this reducer has a default state which the application will get started
 // 2) our reducer function returns a state even if there's no actions or if the action is not recognize 
 let reducer = (state = {name: 'Anonymous'}, action) => {
+  console.log('New Action', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state;
+  }
   return state;
 }
 const store = redux.createStore(reducer);
 
 // This getState method returns our object. in this case the default state is name: anonymous
-const currentState = store.getState();
-console.log('Current State: ' + currentState);
+let currentState = store.getState();
+console.log('Current State: ', currentState);
+
+// All actions are objects
+// const action = {
+//   type: 'CHANGE_NAME',
+//   name: 'luigi'
+// }
+// dispatch method just required one property which is an action
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'luigi'
+});
+
+console.log('Name should be luigi: ',store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -11,11 +11,28 @@ const stateDefault = {
 
 let reducer = (state = stateDefault, action) => {
 
-  return state;
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
 }
 
 const store = redux.createStore(reducer);
 
 const currentState = store.getState();
 
-console.log(currentState);
+console.log('Current state ', currentState);
+
+const action = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'something to find'
+}
+
+store.dispatch(action);
+
+console.log('searchText should be "something to find"', store.getState());
